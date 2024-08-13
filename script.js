14ac81c256a7a6e6f3e3c68dcdfd1c2400e381cf
André Peixoto Lopes <147782790+Andre-peixoto-lopes@users.noreply.github.com>
2024-08-13 18:58:55 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const relogio = setInterval(function time() {
     let sec = dateToday.getSeconds();
 
     if (hr < 10) hr = '0' + hr;
-    if (min < 10) min = '0' + hr;
+    if (min < 10) min = '0' + min;
     if (sec < 10) sec = '0' + sec;
 
     horas.textContent = hr;
